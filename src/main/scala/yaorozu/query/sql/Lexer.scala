216a08830eb1c278a4bbe8d0212ab6b504cc83a4
nekikara <nekikara@gmail.com>
2020-01-20 07:20:32 +0100

    WIP

--- src/main/scala/yaorozu/query/sql/Lexer.scala
+++ src/main/scala/yaorozu/query/sql/Lexer.scala
@@ -12,23 +12,57 @@ object Lexer {
   def listTokens(sql: String): List[String] = {
     @tailrec
     def loop(stock: List[String], lexer: Lexer): List[String] = {
-      if (lexer.isOver) stock
-      else {
-        lexer.char() match {
-          case ' ' | '\t' | '\n' | '\r' =>
-
-
+      if (lexer.isOver) {
+        stock
+      } else {
+        val c = lexer.char()
+        c match {
+          case _ if isWS(c) => {
+            val (wsrest, newLexer) = ws(c, lexer.increment())
+            loop(stock :+ wsrest, newLexer)
+          }
+          case _ => {
+            val (wd, newLexer) = word(c, lexer.increment())
+            loop(stock :+ wd, newLexer)
+          }
         }
 
-        loop(stock, lexer.increment())
       }
     }
     val lexer = Lexer(sql)
     loop(List.empty[String], lexer)
   }
 
-  def ws(lexer: Lexer): (Lexer, String) = {
-    
-
+  def ws(start: Char, lexer: Lexer): (String, Lexer) = {
+    @tailrec
+    def loop(str: String, lex: Lexer): (String, Lexer) = {
+      if (lex.isOver) (str, lex)
+      else {
+        val c = lex.char()
+        c match {
+          case _ if isWS(c) => loop(str :+ c, lex.increment())
+          case _ => (str, lex)
+        }
+      }
+    }
+    loop(start.toString, lexer)
   }
+
+  def word(start: Char, lexer: Lexer): (String, Lexer) = {
+    @tailrec
+    def loop(str: String, lex: Lexer): (String, Lexer) = {
+      if (lex.isOver) (str, lex)
+      else {
+        val c = lex.char()
+        c match {
+          case _ if isWord(c) => loop(str :+ c, lex.increment())
+          case _ => (str, lex)
+        }
+      }
+    }
+    loop(start.toString, lexer)
+  }
+
+  def isWS(c: Char): Boolean = c == ' ' || c == '\t' || c == '\n' || c == '\r'
+  def isWord(c: Char): Boolean = ('a' <= c  && c <= 'z') || ('A' <= c && c <= 'Z')
 }
--- src/test/scala/yaorozu/query/sql/LexerSpec.scala
+++ src/test/scala/yaorozu/query/sql/LexerSpec.scala
@@ -3,7 +3,22 @@ package yaorozu.query.sql
 import org.scalatest._
 
 class LexerSpec extends FunSuite with DiagrammedAssertions {
-  test("Interpreter should return a Literal that is an Int") {
-    val tokens = Lexer.listTokens("CREATE DATABASE test")
+  test("Lexer should return a list of String from a query statement") {
+    val actual = Lexer.listTokens("CREATE DATABASE    \n test")
+    val expect = List("CREATE", " ",  "DATABASE", "    \n ", "test")
+    assert(actual == expect)
+  }
+  test("Lexer should know ") {
+    val actual = Lexer("h", 1).isOver
+    val expect = true
+    assert(actual == expect)
+  }
+  test("Lexer should know a space") {
+    val actual = Lexer.isWS(' ')
+    val expect = true
+    assert(actual == expect)
+    val actual2 = Lexer.isWord(' ')
+    val expect2 = false
+    assert(actual2 == expect2)
   }
 }
